52622e319717cc520d2c48e69f4f4daad5395b86
Greg <github@gregbarendt.com>
2010-12-17 14:23:51 -0500

    Fixed create_path_if_not_exists to be OS-agnostic.

--- rbs/Files.py
+++ rbs/Files.py
@@ -4,15 +4,12 @@ def create_path_if_not_exists(path):
     """Make sure the entire filepath exists, creating directories along the way
     if needed.
 
-    WARNING: This is almost completely untested and won't work on Windows.
-
     path -- The filesystem path to create.
 
-    TODO: Make this OS-agnostic.
     """
-    components = path.split("/")
+    components = path.split(os.sep)
     for i in xrange(len(components)):
-        path = "/".join(components[:i+1])
+        path = os.sep.join(components[:i+1])
         if not os.path.exists(path):
             os.mkdir(path)
     return True
