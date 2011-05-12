d6858e817c0484a08df824d796bdd44a925be312
Greg Barendt <github@gregbarendt.com>
2011-05-11 23:06:25 -0400

    added population_type filters to load_sequences

--- rbs/Phage.py
+++ rbs/Phage.py
@@ -1,17 +1,30 @@
 from contextlib import closing
 import sqlite3
 
+from rbs.Constants import sd_variants_medium
 from rbs.Exceptions import RBSError
 
 class PhageNoSequencesError(Exception):
-    def __init__(self, msg):
-        self.msg = msg
+    pass
 
-def load_sequences(db, accession_id, annotated_yn='N'):
+def load_sequences(db, accession_id, annotated_yn='N', population_type="all"):
     sql = """SELECT REPLACE(rbs,"T","U") FROM sequences 
              WHERE accession_id = ? 
              AND LENGTH(rbs) = 18
              AND is_annotated_as_rbs = ?"""
+    if population_type == "no_sd":
+        for variant in sd_variants_medium[4]:
+            sql += " AND rbs NOT LIKE %s" % (
+                "'%"+variant.replace("U", "T")+"%'")
+    elif population_type == "only_sd":
+        sql += " AND ("
+        pieces = list()
+        for variant in sd_variants_medium[4]:
+            pieces.append(
+                "rbs LIKE %s" % (
+                    "'%"+variant.replace("U","T")+"%'"))
+        sql += " OR ".join(pieces)
+        sql += ")"
     db = sqlite3.connect(db)
     db.row_factory = sqlite3.Row
     with closing(db.cursor()) as cursor:
