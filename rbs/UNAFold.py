d566b4ca32d9cc98138f09e9f0fad0a873dcc0c9
Greg Barendt <github@gregbarendt.com>
2011-05-25 01:11:06 -0400

    Added a dG property.

--- rbs/UNAFold.py
+++ rbs/UNAFold.py
@@ -6,7 +6,8 @@ from Bio.RNA.SecstrucParsers import *
 class UnafoldSequence(object):
 
     def __init__(self, sequence):
-        self.sequence = sequence    
+        self._dG = None
+        self.sequence = sequence
 
     def fold(self):
         with open('/tmp/sequence.seq', 'wb') as f:
@@ -20,5 +21,11 @@ class UnafoldSequence(object):
             ).communicate()[0]
         fnull.close()
         self.seq, self.secstruc = parse_ct(open("/tmp/sequence.ct", "rU"))
-    
-    
+
+    def get_dg(self):
+        if not self._dG:
+            with open('/tmp/sequence.dG', 'r') as f:
+                contents = f.read()
+                self._dG = float(contents.split("\n")[1].split("\t")[1])
+        return self._dG
+    dG = property(get_dg)
